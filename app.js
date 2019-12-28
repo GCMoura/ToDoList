ac8cfc794816f0e6de1d0705535fde25946cfd3f
GCMoura <51333439+GCMoura@users.noreply.github.com>
2019-12-27 22:20:56 -0300

    27/12

--- app.js
+++ app.js
@@ -3,29 +3,60 @@ var right = document.querySelector('#right ul')
 var button = document.querySelector('.input-group-append button')
 var activities = []
 
-button.onclick = show;
+var activities = JSON.parse(localStorage.getItem('list_activities')) || []
 
-function show() {
-    var activity = input.value
+button.onclick = addActivity;
 
-    activities.push(activity)
+buttonEmail.onclick = sendEmail;
+
+function render() {
 
     right.innerHTML = ""
 
     for (activity of activities) {
         var listElement = document.createElement('li')
         var textActivity = document.createTextNode(activity)
+
         listElement.appendChild(textActivity)
 
-        var link = document.createElement('a')
-        link.setAttribute('href', '#')
-        var textLink = document.createTextNode('   Excluir')
-        link.appendChild(textLink)
-        listElement.appendChild(link)
+        var buttonDelete = document.createElement('button')
+        buttonDelete.className = 'btn btn-danger btnDelete'
+
+        var pos = activities.indexOf(activity)
+        buttonDelete.setAttribute('onclick', 'deleteActivity(' + pos + ')')
+
+        var textButton = document.createTextNode('Excluir')
+        buttonDelete.appendChild(textButton)
+
+        listElement.appendChild(buttonDelete)
 
         right.appendChild(listElement)
-
     }
+}
+
+render()
+
+function addActivity() {
+    var activity = input.value
+
+    activities.push(activity)
+
+    render()
+    saveStorage()
 
     input.value = "";
+    input.focus()
+}
+
+function deleteActivity(pos) {
+    activities.splice(pos, 1)
+
+    render()
+    saveStorage()
+}
+
+function saveStorage() {
+    localStorage.setItem('list_activities', JSON.stringify(activities));
+
+    //console.log(window.localStorage.getItem('list_activities'))
 }
